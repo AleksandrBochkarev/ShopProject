16577f626433666e72476210d88881f3d71e292e
Vadim.Vassilevits <amistion@gmail.com>
2020-05-22 20:53:57 +0300

    product properties

--- Domain/Product.cs
+++ Domain/Product.cs
@@ -6,5 +6,14 @@ namespace Domain
 {
     class Product
     {
+        public int ProductId { get; set; }
+  
+        public string ProductName { get; set; }
+
+        public decimal ProductPrice { get; set; }
+
+        public int ProductQuantity { get; set; }
+
+
     }
 }
