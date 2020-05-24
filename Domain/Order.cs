46cd87ed704c785c5b3ce6b33e4b0e97f03ae521
AleksandrBochkarev <aleksandrbotskarjov@gmail.com>
2020-05-24 22:47:08 +0300

    Order and UserOrders fix

--- Domain/Order.cs
+++ Domain/Order.cs
@@ -8,7 +8,9 @@ namespace Domain
         public int OrderId { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public decimal TotalPrice { get; set; }
+        public int UserId { get; set; }
 
+        public User? User { get; set; }
         public ICollection<OrderProducts>? ProductsOrdered { get; set; }
     }
 }
--- Domain/UserOrders.cs
+++ Domain/UserOrders.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
 
 namespace Domain
 {
     public class UserOrders
     {
+        public int UserOrdersId { get; set; }
+
+
+        public int OrderId { get; set; }
+
+        public Order? Order { get; set; }
+
+        public int UserId { get; set; }
+
+        public User? User { get; set; }
     }
 }
